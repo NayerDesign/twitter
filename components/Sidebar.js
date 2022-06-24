c597a6fb3685b2fcb795abb31ea5c0043e013ad1
Nayer Raja <creativz.design@yahoo.com>
2022-06-24 15:02:41 +0500

    Initial commit

---
+++ components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div>Sidebar
+
+        {/* Twitter Log */}
+        <div className="">
+        {/* <Image width="50" height="50" src="https://toppng.com/uploads/preview/twitter-logo-11549680523gyu1fhgduu.png" ></Image> */}
+
+        <img src="https://rg.gy/ulxxee" alt="" />
+            
+        </div>
+        {/* Menu */}
+        {/* Button */}
+        {/* Min_Portfolio */}
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    // images:{
+    //   domains:['https://www.pngegg.com/en/search?q=TWITTER']
+    // }
 }
 
 module.exports = nextConfig
